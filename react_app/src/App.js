eb17213706c58eed7737ee9aaa6fd0ad0f890b5f
dcodewizard <am.anammaqbool@gmail.com>
2024-07-26 05:49:20 +0500

    React App Initial commit

---
+++ react_app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
